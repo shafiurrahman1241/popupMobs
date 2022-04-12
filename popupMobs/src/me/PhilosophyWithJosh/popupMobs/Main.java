629967c5e08cf123443fb167d20ca65e736c6a92
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-04-11 22:08:39 -0500

    the first commit of the popupMob plugin

---
+++ popupMobs/src/me/PhilosophyWithJosh/popupMobs/Main.java
@@ -0,0 +1,13 @@
+package me.PhilosophyWithJosh.popupMobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class Main extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+}
---
+++ popupMobs/src/me/PhilosophyWithJosh/popupMobs/listeners/blockListener.java
@@ -0,0 +1,23 @@
+package me.PhilosophyWithJosh.popupMobs.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import me.PhilosophyWithJosh.popupMobs.Main;
+
+public class blockListener implements Listener
+{
+	private Main plugin;
+	
+	public blockListener(Main plugin)
+	{
+		this.plugin = plugin;
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+	
+	public void onMove(PlayerMoveEvent pme)
+	{	
+		
+	}
+}
