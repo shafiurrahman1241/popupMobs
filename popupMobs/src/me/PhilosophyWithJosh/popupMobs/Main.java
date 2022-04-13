bd60bc84b543b885f65017d11ca616282a247ef6
Shafiur Rahman Bhauyan <shafiurrahman1241@gmail.com>
2022-04-13 18:03:31 -0500

    added the block materials and the spawning objects

--- popupMobs/src/me/PhilosophyWithJosh/popupMobs/Main.java
+++ popupMobs/src/me/PhilosophyWithJosh/popupMobs/Main.java
@@ -2,12 +2,14 @@ package me.PhilosophyWithJosh.popupMobs;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.PhilosophyWithJosh.popupMobs.listeners.blockListener;
+
 
 public class Main extends JavaPlugin
 {
 	@Override
 	public void onEnable()
 	{
-		
+		new blockListener(this);
 	}
 }
--- popupMobs/src/me/PhilosophyWithJosh/popupMobs/listeners/blockListener.java
+++ popupMobs/src/me/PhilosophyWithJosh/popupMobs/listeners/blockListener.java
@@ -1,6 +1,14 @@
 package me.PhilosophyWithJosh.popupMobs.listeners;
 
+import java.util.ArrayList;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -10,15 +18,46 @@ public class blockListener implements Listener
 {
 	@SuppressWarnings("unused")
 	private Main plugin;
+	private ArrayList<Material> blocklist = new ArrayList<Material>();
+	private ArrayList<EntityType> entitylist = new ArrayList<EntityType>();
+
 	
 	public blockListener(Main plugin)
 	{
 		this.plugin = plugin;
 		Bukkit.getPluginManager().registerEvents(this, plugin);
+		
+		// lists of blocks to checkif the player is standing on it or not
+		blocklist.add(Material.OBSIDIAN);
+		blocklist.add(Material.CACTUS);
+		blocklist.add(Material.SAND);
+		blocklist.add(Material.OAK_LOG);
+		blocklist.add(Material.DIAMOND_ORE);
+
+		// LIST OF CORRESPONDING MOBS TO SPAWN
+		entitylist.add(EntityType.COW);
+		entitylist.add(EntityType.BEE);
+		entitylist.add(EntityType.WOLF);
+		entitylist.add(EntityType.ZOMBIE);
+		entitylist.add(EntityType.DOLPHIN);
+
+		
 	}
 	
+	@EventHandler
 	public void onMove(PlayerMoveEvent pme)
 	{	
+		Player mb = pme.getPlayer();
+		Location loc = mb.getLocation().clone().subtract(0, 1, 0);
+		Block square = loc.getBlock();
+		Material mat = square.getType();
 		
+		for (int i = 0; i<blocklist.size(); i++) {
+			if (blocklist.get(i)== mat) {
+				mb.getWorld().spawnEntity(loc, entitylist.get(i));
+				mb.sendMessage("Spawning entity");
+				
+			}
+		}
 	}
 }
